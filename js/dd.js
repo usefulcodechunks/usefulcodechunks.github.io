8e6ac2cb7b6d8e0b79a5d10736215975de6061b4
Jean <jt665@cornell.edu>
2020-04-12 23:21:31 -0400

    Final temp update

--- js/dd.js
+++ js/dd.js
@@ -2,11 +2,11 @@
 function getInputValue(){
    // Selecting the input element and get its value
    var inputVal = document.getElementById("myInput").value;
-
+   console.log(inputVal)
    // Displaying the value
    //alert(inputVal);
    document.getElementById('LinkedIn').setAttribute("href",("https://www.linkedin.com/search/results/all/?keywords=").concat(inputVal));
    document.getElementById('crunchbase').setAttribute("href",("https://www.crunchbase.com/textsearch?q=").concat(inputVal));
-   document.getElementById('google').setAttribute("href",("https://www.google.com/search?q=").concat(inputVal));
+   document.getElementById('google').setAttribute("href",("https://www.google.com/search?q=\"").concat(inputVal).concat("\""));
 
 }
