a0c01648c21e2d3429fc8246f09ce0789c89551e
Jean <jt665@cornell.edu>
2022-05-24 17:32:06 -0400

    Reformatting app

---
+++ Streamlitapp/newmaster.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+
+from nodeapp import render_node_app
+from duedillgence import due_dill_app
+
+
+
+
+def initialize_session_state_vars():
+    if 'cycle' not in st.session_state:
+        st.session_state.cycle = 0
+    if 'current_page' not in st.session_state:
+        st.session_state.current_page = "Home"
+
+
+
+
+
+def render_sidebar(sidebar_object):
+    with sidebar_object.expander("Page navigation"):
+        selected_page = st.selectbox('Select a page', ["Home", "Node App", "Due diligence"])
+        st.session_state.current_page = selected_page
+
+def render_page(main_page_object):
+
+    if(main_page_object.session_state.current_page == "Home"):
+        main_page_object.title(main_page_object.session_state.current_page)
+
+    elif(main_page_object.session_state.current_page == "Node App"):
+        main_page_object.title(main_page_object.session_state.current_page)
+        render_node_app(main_page_object)
+
+    elif(main_page_object.session_state.current_page == "Due diligence"):
+        main_page_object.title(main_page_object.session_state.current_page)
+        due_dill_app(main_page_object)
+
+    else:
+        main_page_object.title("404 No Page Found")
+
+render_sidebar(st.sidebar)
+render_page(st)
